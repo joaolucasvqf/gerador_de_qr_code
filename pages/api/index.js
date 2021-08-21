f6052c8fe0fa5e444cf925597efba4a55e417bb7
João Lucas Fonseca <joaolucasvqf@hotmail.com>
2021-08-21 00:28:01 -0300

    initial commit

---
+++ pages/api/index.js
@@ -0,0 +1,10 @@
+import QRCode from "qrcode-svg"
+
+export default(req, res) => {
+    const url = req.query.url
+    const svg = new QRCode(url);
+
+    res.statusCode = 200
+    res.setHeader('content-type', 'image/svg+xml')
+    res.send(svg.svg())
+}
