148ad0134556e39cadbf7c2c713e1d4318ab0a0c
João Lucas Fonseca <joaolucasvqf@hotmail.com>
2021-08-22 00:35:11 -0300

    Pequeno ajuste na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 const Index = () => {
-    return (<h1>QRCode</h1>)
+  
+    const handleClick = (e) => {
+        var mensagem = document.getElementById("msg").value; 
+        window.location.href = "/api?url=" + encodeURI(mensagem)    
+    }
+    return (
+        <>
+            <h1>Insira o a mensagem ou link a ser utilizado no QR-Code:</h1>
+            <br></br>
+            <input name="msg" id="msg"></input>
+            <button type="submit" id="btn" onClick={handleClick}>Gerar QR-Code</button>
+        </>
+    )
 }
 export default Index
