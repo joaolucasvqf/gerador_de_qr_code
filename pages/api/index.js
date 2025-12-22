f1ff4ec0deb2846d10443918db7f1c89e1ed320a
João Lucas Fonseca <joaolucasvqf@hotmail.com>
2025-12-21 22:09:07 -0300

    adding pix feature

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,15 +1,35 @@
 import QRCode from "qrcode-svg"
+// import QrcodePix from 'qrcode-pix'
 
 export default(req, res) => {
-    const url = req.query.url
-    const svg = new QRCode({
-        content: url,
-        padding: 4,
-        ecl: 'Q',
-        join: true
-    });
 
-    res.statusCode = 200
-    res.setHeader('content-type', 'image/svg+xml')
-    res.send(svg.svg())
+    try {
+        var query = req.query
+        // if (query.type == pix){
+        //     const svg = new QrcodePix({
+        //         key: query.url,
+        //         name: query.name || 'Nome do Recebedor',
+        //         city: query.city || 'SAO PAULO',
+        //         amount: query.amount || '0.00',
+        //         txId: query.txId || '***',
+        //     });
+        // } else {
+            const svg = new QRCode({
+                content: query.url,
+                padding: 4,
+                width: 256,
+                height: 256,
+                color: "#000000",
+                background: "#ffffff",
+                ecl: 'Q',
+                join: true
+            });
+        // }
+    
+        res.statusCode = 200
+        res.setHeader('content-type', 'image/svg+xml')
+        res.send(svg.svg())
+    } catch (error) {
+        alert(error);
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,43 @@ const Index = () => {
   
     const handleClick = (e) => {
         var mensagem = document.getElementById("msg").value; 
-        var type = document.querySelector('input[name="qr-type"]:checked').value;
-        if (type == "Link") {
-            window.location.href = "/api?url=" + mensagem;
+        var type = document.getElementsByName('qr-type').forEach(i => {if(i.checked) { return i.value }});
+        if (type == "Pix") {
+            var selectedPixType = document.getElementsByName('pixType').forEach(i => {if(i.checked) { return i.value }});
+            window.location.href = "/api?url=" + mensagem + "&type=pix"
+                + "&pixType=" + selectedPixType;
+        } else {        
+            window.location.href = "/api?url=" + encodeURI(mensagem) + "&type=text";
+        }
+    }
+    const handleQRCodeTypeChange = (e) => {
+        var selectedType = e.target.value;
+        var msgInput = document.getElementById("msgHeader");
+        var divPixType = document.getElementById("pixType");
+        if (selectedType == "Pix") {
+            msgInput.textContent = "Insira a sua chave Pix";
+            divPixType.innerHTML = `
+            <div>
+                <h3>Selecione sua chave pix:</h3>
+                <input type="radio" id="cpf" name="pixType" value="cpf defaultChecked">
+                <label htmlFor="cpf">CPF</label>
+
+                <input type="radio" id="email" name="pixType" value="email">
+                <label htmlFor="email">Email</label>
+
+                <input type="radio" id="phone" name="pixType" value="phone">
+                <label htmlFor="phone">Telefone</label>
+            <div>
+            `;
+            //Sets the cpf field as selected by default
+            var pixTypeField = document.getElementsByName("pixType");
+            pixTypeField[0].checked = true;
+        } else if (selectedType == "Link") {
+            msgInput.textContent = "Insira o seu link (URL)";
+            divPixType.innerHTML = "";
         } else {
-            window.location.href = "/api?url=" + encodeURI(mensagem);
+            msgInput.textContent = "Insira o seu texto";
+            divPixType.innerHTML = "";
         }
     }
     return (
@@ -17,18 +49,23 @@ const Index = () => {
 
             <h3>Deseja gerar um link ou texto?</h3>
 
-            <input name="qr-type" type="radio" id="Link" value="Link" checked></input>
-            <label for="Link">Link</label>
+            <input name="qr-type" type="radio" id="Link" value="Link" onClick={handleQRCodeTypeChange} defaultChecked></input>
+            <label htmlFor="Link">Link</label>
 
-            <input name="qr-type" type="radio" id="Texto" value="Texto"></input>
-            <label for="Texto">Texto</label>
+            <input name="qr-type" type="radio" id="Texto" value="Texto" onClick={handleQRCodeTypeChange} ></input>
+            <label htmlFor="Texto">Texto</label>
+            
+            <input name="qr-type" type="radio" id="Pix" value="Pix" onClick={handleQRCodeTypeChange} ></input>
+            <label htmlFor="Pix">Pix</label>
 
             <br></br>
 
-            <h3>Seu texto do QR-Code</h3>
+            <h3 id="msgHeader">Seu texto do QR-Code</h3>
             
             <input name="msg" id="msg"></input>
             <button type="submit" id="btn" onClick={handleClick}>Gerar QR-Code</button>
+
+            <div id="pixType"></div>
         </>
     )
 }
